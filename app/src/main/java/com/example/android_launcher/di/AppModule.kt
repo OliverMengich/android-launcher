c641111c6627e33b73b729c1fbb08133a03a7fda
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-08-07 11:35:33 +0300

    feat: app module dependinces

--- app/src/main/java/com/example/android_launcher/di/AppModule.kt
+++ app/src/main/java/com/example/android_launcher/di/AppModule.kt
@@ -1,31 +1,48 @@
 package com.example.android_launcher.di
 
 import androidx.room.Room
+import com.example.android_launcher.MainViewModel
 import com.example.android_launcher.data.local.AppsDao
-import com.example.android_launcher.data.local.AppsDatabase
+import com.example.android_launcher.data.local.CalendarDao
+import com.example.android_launcher.data.local.LauncherDatabase
+import com.example.android_launcher.data.local.ListTypeConverter
 import com.example.android_launcher.data.repository.AppsRepositoryImpl
+import com.example.android_launcher.data.repository.CalendarRepositoryImpl
 import com.example.android_launcher.domain.repository.AppsRepository
+import com.example.android_launcher.domain.repository.CalendarRepository
 import com.example.android_launcher.presentation.screens.home.SharedViewModel
-import com.example.android_launcher.presentation.screens.home.apps.BlockedAppViewModel
-import com.example.android_launcher.presentation.screens.home.calendar.CalendarViewModel
+import com.example.android_launcher.presentation.screens.home.home.apps.BlockingAppViewModel
+import com.example.android_launcher.presentation.screens.home.home.calendar.CalendarViewModel
+import com.example.android_launcher.presentation.screens.home.settings.SettingsViewModel
+import com.example.android_launcher.presentation.screens.onboarding.OnboardingViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 //
 val appModule = module {
     single {
-        Room.databaseBuilder(context = get(), klass = AppsDatabase::class.java, name = "apps_db")
+        Room.databaseBuilder(context = get(), klass = LauncherDatabase::class.java, name = "apps_db")
+//            .addTypeConverter(ListTypeConverter())
+            .fallbackToDestructiveMigration(false)
             .build()
     }
 
     single <AppsDao> {
-        get<AppsDatabase>().appsDao()
+        get< LauncherDatabase>().appsDao()
+    }
+
+    single <CalendarDao> {
+        get<LauncherDatabase>().calendarDao()
     }
 
     single <AppsRepository> { AppsRepositoryImpl(get()) }
+    single<CalendarRepository> { CalendarRepositoryImpl(get()) }
 
     viewModelOf(::CalendarViewModel)
-    viewModelOf(::BlockedAppViewModel)
+    viewModelOf(::BlockingAppViewModel)
+    viewModelOf(::OnboardingViewModel)
+    viewModelOf(::MainViewModel)
+    viewModelOf(::SettingsViewModel)
     viewModel { SharedViewModel(get(),get()) }
 
 }
