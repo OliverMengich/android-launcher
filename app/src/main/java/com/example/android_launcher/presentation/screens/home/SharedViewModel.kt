7b09d7cecd2737e398712cb40c534b5f05a1bfc8
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-08-13 08:10:23 +0300

    feat: sharedviewmodel component

--- app/src/main/java/com/example/android_launcher/presentation/screens/home/SharedViewModel.kt
+++ app/src/main/java/com/example/android_launcher/presentation/screens/home/SharedViewModel.kt
@@ -3,20 +3,31 @@ package com.example.android_launcher.presentation.screens.home
 import android.app.usage.UsageStats
 import android.app.usage.UsageStatsManager
 import android.content.Context
+import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.android_launcher.BlockedAppActivity
+import com.example.android_launcher.CAMERA_APP_PACKAGE
+import com.example.android_launcher.CLOCK_APP_PACKAGE
+import com.example.android_launcher.IS_LOGGED_IN_KEY
+import com.example.android_launcher.PHONE_APP_PACKAGE
+import com.example.android_launcher.dataStore
 import com.example.android_launcher.domain.models.App
 import com.example.android_launcher.domain.repository.AppsRepository
+import com.example.android_launcher.utils.isDatePassed
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
-import kotlin.random.Random
 
 class SharedViewModel(private val appsRepository: AppsRepository, val context: Context): ViewModel() {
     val pm: PackageManager? = context.packageManager
@@ -25,6 +36,16 @@ class SharedViewModel(private val appsRepository: AppsRepository, val context: C
 
     private val _pinnedApps = MutableStateFlow<List<App>>(emptyList())
     val pinnedApps: StateFlow<List<App>> = _pinnedApps.asStateFlow()
+
+
+    val batteryInfo = appsRepository.batteryInfo
+
+    private val _navigateToBlockedAppPage = Channel<App?>()
+    val navigateToBlockedAppPage = _navigateToBlockedAppPage.receiveAsFlow()
+
+    private val _navigateToBlockingAppPage = Channel<App?>()
+    val navigateToBlockingAppPage = _navigateToBlockingAppPage.receiveAsFlow()
+
     fun getPinnedApps(){
         val pinApps = appsRepository.getPinnedApps()
         _pinnedApps.value = pinApps
@@ -39,27 +60,44 @@ class SharedViewModel(private val appsRepository: AppsRepository, val context: C
     }
 
     fun getApps(){
-        Log.d("getting_app","before fetched")
         val ftApps = appsRepository.getAllApps()
-        Log.d("getting_app","apps=${ftApps.size}")
         if (ftApps.isEmpty()){
-            loadApps()
+            viewModelScope.launch(Dispatchers.IO) {
+                loadApps()
+            }
         }else{
             _apps.value = ftApps
         }
     }
-    suspend fun blockAppFc(appId: Int){
-        appsRepository.blockApp(appId)
-    }
-    fun pinApp(appId: Int){
+
+    fun blockUnblockAppFc(app: App,blocked: Int){
         viewModelScope.launch(Dispatchers.IO) {
-            appsRepository.pinApp(appId)
-            getPinnedApps()
+            if (app.isPinned == true) {
+                appsRepository.pinUnpinApp(app.packageName, 0)
+            }
+            appsRepository.blockUnblockApp(app.packageName, blocked, blockReleaseDate = "")
+            getApps()
         }
     }
-    suspend fun hideAppFc(appId: Int){
-        appsRepository.hideApp(appId)
-        getApps()
+    fun pinUnpinApp(app: App, pinned: Int){
+        if (app.isBlocked==true){
+            Toast.makeText(context,"You cannot pin a blocked app", Toast.LENGTH_LONG).show()
+            return
+        }
+        viewModelScope.launch(Dispatchers.IO) {
+            appsRepository.pinUnpinApp(app.packageName,pinned)
+            getPinnedApps()
+            getApps()
+        }
+    }
+    fun hideUnhideAppFc(app: App,hidden: Int){
+        viewModelScope.launch(Dispatchers.IO) {
+            if (app.isPinned == true) {
+                appsRepository.pinUnpinApp(app.packageName, 0)
+            }
+            appsRepository.hideUnhideApp(app.packageName, hidden)
+            getApps()
+        }
     }
     fun addNewInstalledApp(app: App){
         viewModelScope.launch {
@@ -73,7 +111,7 @@ class SharedViewModel(private val appsRepository: AppsRepository, val context: C
             getApps()
         }
     }
-    fun loadApps(){
+    suspend fun loadApps(){
         val packages = mutableListOf<ApplicationInfo>()
         val allPacs = pm?.getInstalledApplications(PackageManager.GET_META_DATA)
         if (allPacs != null) {
@@ -86,15 +124,45 @@ class SharedViewModel(private val appsRepository: AppsRepository, val context: C
         }
         packages.sortBy { pm?.getApplicationLabel(it).toString() }
         val newApps = packages.map { ap ->
-            App(
-                id = Random.nextInt(100000, 1000000),
-                packageName = ap.packageName,
-                name = pm?.getApplicationLabel(ap).toString(),
-            )
+            Log.d("package_name","package name=${ap.packageName}")
+            if (ap.packageName.contains("camera",true)){
+                context.dataStore.edit { st ->
+                    st[CAMERA_APP_PACKAGE] = ap.packageName
+                }
+            }else if (ap.packageName.contains("dialer",true)){
+                context.dataStore.edit { st ->
+                    st[PHONE_APP_PACKAGE] = ap.packageName
+                }
+            }else if (ap.packageName.contains("clock",true)){
+                context.dataStore.edit { st ->
+                    st[CLOCK_APP_PACKAGE] = ap.packageName
+                }
+            }
+            val category = getCategoryName(ap.category)
+//            App(packageName = ap.packageName, category = category, name = pm?.getApplicationLabel(ap).toString())
+            App(packageName = ap.packageName, category = category, name = ap.loadLabel(pm as PackageManager).toString())
         }
         appsRepository.insertApps(newApps)
         _apps.value = newApps
-
+    }
+    private fun getCategoryName(category: Int): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            when (category) {
+                ApplicationInfo.CATEGORY_GAME -> "Game"
+                ApplicationInfo.CATEGORY_AUDIO -> "Audio"
+                ApplicationInfo.CATEGORY_VIDEO -> "Video"
+                ApplicationInfo.CATEGORY_IMAGE -> "Image"
+                ApplicationInfo.CATEGORY_SOCIAL -> "Social"
+                ApplicationInfo.CATEGORY_NEWS -> "News"
+                ApplicationInfo.CATEGORY_MAPS -> "Maps"
+                ApplicationInfo.CATEGORY_PRODUCTIVITY -> "Productivity"
+                ApplicationInfo.CATEGORY_ACCESSIBILITY -> "Accessibility"
+                ApplicationInfo.CATEGORY_UNDEFINED -> "Undefined"
+                else -> "Other"
+            }
+        } else {
+            "Not Available (API < 26)"
+        }
     }
 
     fun getAppUsageStats(context: Context, packageName: String): UsageStats? {
@@ -113,13 +181,38 @@ class SharedViewModel(private val appsRepository: AppsRepository, val context: C
         return stats.find { it.packageName == packageName }
     }
 
-    fun launchApp(packageName: String){
-        Log.d("pkgNme","package name=$packageName")
-        val intent = pm?.getLaunchIntentForPackage(packageName)
-        if (intent != null){
-            context.startActivity(intent)
-        }else{
-            Toast.makeText(context, "Can't Launch this app", Toast.LENGTH_SHORT).show()
+     fun launchApp(app: App){
+         Log.d("package_name",app.packageName)
+         viewModelScope.launch(Dispatchers.IO) {
+             if (app.isBlocked==true){
+                //navigate to the app blocked page.
+                 Log.d("passed_time","app=$app")
+                if (app.blockReleaseDate !=null &&isDatePassed(app.blockReleaseDate )){
+                    Log.d("passed_time","time is passed")
+                    appsRepository.blockUnblockApp(app.packageName,0,null)
+                    getApps()
+                    val intent = pm?.getLaunchIntentForPackage(app.packageName)
+                    if (intent != null){
+                        context.startActivity(intent)
+                    }else{
+                        Toast.makeText(context, "Can't Launch this app", Toast.LENGTH_SHORT).show()
+                    }
+                }else {
+                    val intent = Intent(context, BlockedAppActivity::class.java).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        putExtra("app_name", app.name)
+                        putExtra("release_date", app.blockReleaseDate)
+                    }
+                    context.startActivity(intent)
+                }
+            }else {
+                 val intent = pm?.getLaunchIntentForPackage(app.packageName)
+                 if (intent != null) {
+                     context.startActivity(intent)
+                 } else {
+                     Toast.makeText(context, "Can't Launch this app", Toast.LENGTH_SHORT).show()
+                 }
+             }
         }
     }
 }
