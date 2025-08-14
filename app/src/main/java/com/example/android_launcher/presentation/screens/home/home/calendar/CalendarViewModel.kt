c6525eaef519e60a9d62c75ea44739f8544f9204
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-08-14 08:17:40 +0300

    chore: calendar view model item

--- app/src/main/java/com/example/android_launcher/presentation/screens/home/home/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/example/android_launcher/presentation/screens/home/home/calendar/CalendarViewModel.kt
@@ -1,21 +1,47 @@
-package com.example.android_launcher.presentation.screens.home.calendar
+package com.example.android_launcher.presentation.screens.home.home.calendar
 
-import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android_launcher.domain.models.Event
+import com.example.android_launcher.domain.repository.CalendarRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.temporal.ChronoUnit
 import java.time.temporal.TemporalAdjusters
+import java.util.Calendar
 
-class CalendarViewModel: ViewModel() {
+class CalendarViewModel(private val calendarRepository: CalendarRepository): ViewModel() {
     private val _dates = MutableStateFlow< List<List<LocalDate>>>(emptyList())
     val dates: StateFlow<List<List<LocalDate>>> = _dates.asStateFlow()
 
-    fun generateDays() {
-        val today = LocalDate.now()
+    private val _todayEvents = MutableStateFlow<List<Event>>(emptyList())
+    val todayEvents: StateFlow<List<Event>> = _todayEvents.asStateFlow()
+
+    init {
+        viewModelScope.launch(context=Dispatchers.IO) {
+            val calendar = Calendar.getInstance()
+            val startDay = calendar.apply {
+                set(Calendar.HOUR_OF_DAY,0)
+                set(Calendar.MINUTE,0)
+                set(Calendar.SECOND,0)
+                set(Calendar.SECOND,0)
+                set(Calendar.MILLISECOND,0)
+            }.timeInMillis
+            getTodayEvents(startDay)
+        }
+    }
+
+    suspend fun getTodayEvents(startDay: Long){
+        val tdEvs = calendarRepository.getTodayEvents(startDay)
+        _todayEvents.value = tdEvs
+    }
+
+    fun generateDays(dt: LocalDate?) {
+        val today = dt?:LocalDate.now()
         val start = today.minusDays(14)
         val end = today.plusDays(21)
 
@@ -25,15 +51,15 @@ class CalendarViewModel: ViewModel() {
 
         result.forEach { println(it) }
     }
-}
-fun eachWeekUntil(start: LocalDate, end: LocalDate): List<List<LocalDate>> {
-    val firstWeekStart = start.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY))
-    val days = generateSequence(firstWeekStart) { it.plusDays(1) }
-        .takeWhile { !it.isAfter(end) }
-        .toList()
-    return days.chunked(7)
-//    val firstWeekStart = this.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY))
-//    return generateSequence(firstWeekStart) { it.plusWeeks(1) }
-//        .takeWhile { !it.isAfter(end) }
-//        .toList()
+
+    suspend fun newEvent(e: Event){
+        calendarRepository.insertEvent(e)
+    }
+    private fun eachWeekUntil(start: LocalDate, end: LocalDate): List<List<LocalDate>> {
+        val firstWeekStart = start.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY))
+        val days = generateSequence(firstWeekStart) { it.plusDays(1) }
+            .takeWhile { !it.isAfter(end) }
+            .toList()
+        return days.chunked(7)
+    }
 }
