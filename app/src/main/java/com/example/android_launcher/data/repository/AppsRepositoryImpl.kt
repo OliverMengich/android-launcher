a9d50244a98d6e370de4b9d8e1256a7732ee6fa3
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-08-26 09:45:20 +0300

    feat: implements, blocking, unblocking, hidding, pining apps

--- app/src/main/java/com/example/android_launcher/data/repository/AppsRepositoryImpl.kt
+++ app/src/main/java/com/example/android_launcher/data/repository/AppsRepositoryImpl.kt
@@ -2,11 +2,21 @@ package com.example.android_launcher.data.repository
 
 import com.example.android_launcher.data.local.AppsDao
 import com.example.android_launcher.domain.models.App
+import com.example.android_launcher.domain.models.BatteryInfo
 import com.example.android_launcher.domain.repository.AppsRepository
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class AppsRepositoryImpl(private val appsDao: AppsDao): AppsRepository {
+
+    override val batteryInfo = MutableStateFlow<BatteryInfo?>(null)
+//    override val batteryInfo: StateFlow<BatteryInfo?> = _batteryInfo.asStateFlow()
+
     override fun getAllApps(): List<App> {
         return appsDao.getAllApps()
+    }
+
+    override fun batteryInfoReceiver(batteryInfoVal: BatteryInfo) {
+        batteryInfo.value = batteryInfoVal
     }
 
     override fun insertApps(apps: List<App>) {
@@ -19,16 +29,28 @@ class AppsRepositoryImpl(private val appsDao: AppsDao): AppsRepository {
         return appsDao.getPinnedApps()
     }
 
-    override suspend fun blockApp(appId: Int) {
-        appsDao.blockApp(appId)
+    override fun getHiddenApps(): List<App> {
+        return appsDao.getHiddenApps()
     }
 
-    override suspend fun pinApp(appId: Int) {
-        appsDao.pinApp(appId)
+    override fun getBlockedApps(): List<App> {
+        return appsDao.getBlockedApps()
     }
 
-    override suspend fun hideApp(appId: Int) {
-        appsDao.hideApp(appId)
+    override suspend fun blockUnblockApp(
+        packageName: String,
+        blocked: Int,
+        blockReleaseDate: String?
+    ) {
+        appsDao.blockUnblockApp(packageName,blocked, blockReleaseDate = blockReleaseDate)
+    }
+
+    override suspend fun pinUnpinApp(packageName: String,pinned: Int) {
+        appsDao.pinApp(packageName,pinned)
+    }
+
+    override suspend fun hideUnhideApp(packageName: String, hidden: Int) {
+        appsDao.hideUnhideApp(packageName,hidden)
     }
 
     override fun newAppInstalled(app: App) {
--- app/src/main/java/com/example/android_launcher/domain/repository/AppsRepository.kt
+++ app/src/main/java/com/example/android_launcher/domain/repository/AppsRepository.kt
@@ -1,14 +1,20 @@
 package com.example.android_launcher.domain.repository
 
 import com.example.android_launcher.domain.models.App
+import com.example.android_launcher.domain.models.BatteryInfo
+import kotlinx.coroutines.flow.MutableStateFlow
 
 interface AppsRepository {
+    val batteryInfo: MutableStateFlow<BatteryInfo?>
     fun getAllApps(): List<App>
     fun insertApps(apps: List<App>)
     fun getPinnedApps(): List<App>
-    suspend fun blockApp(appId: Int)
-    suspend fun pinApp(appId: Int)
-    suspend fun hideApp(appId: Int)
+    fun getHiddenApps(): List<App>
+    fun getBlockedApps(): List<App>
+    suspend fun blockUnblockApp(packageName: String, blocked: Int, blockReleaseDate: String?)
+    suspend fun pinUnpinApp(packageName: String,pinned: Int)
+    suspend fun hideUnhideApp(packageName: String,hidden: Int)
     fun newAppInstalled(app: App)
     suspend fun removeUninstalled(packageName: String)
+    fun batteryInfoReceiver(batteryInfo: BatteryInfo)
 }
