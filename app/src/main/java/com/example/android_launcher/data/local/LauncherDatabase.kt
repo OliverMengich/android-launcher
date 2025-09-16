5bb42678880b9dc3f0908f4d014507097781f4d8
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-09-16 14:42:12 +0300

    feat: adds list converter  and renames db

--- app/src/main/java/com/example/android_launcher/data/local/LauncherDatabase.kt
+++ app/src/main/java/com/example/android_launcher/data/local/LauncherDatabase.kt
@@ -2,12 +2,13 @@ package com.example.android_launcher.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.android_launcher.domain.models.App
 import com.example.android_launcher.domain.models.Event
 
-
-@Database(entities = [App::class, Event::class], version = 1)
-abstract class AppsDatabase: RoomDatabase() {
+@Database(entities = [Event::class, App::class], version = 1)
+@TypeConverters(ListTypeConverter::class)
+abstract class LauncherDatabase: RoomDatabase() {
     abstract fun appsDao(): AppsDao
     abstract fun calendarDao(): CalendarDao
 }
