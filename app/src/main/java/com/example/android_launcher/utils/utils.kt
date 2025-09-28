9159f1c00ff16fe57f55d2574fd82b000aba8045
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-09-28 13:54:11 +0300

    chore: utility functions

--- app/src/main/java/com/example/android_launcher/utils/utils.kt
+++ app/src/main/java/com/example/android_launcher/utils/utils.kt
@@ -2,12 +2,17 @@ package com.example.android_launcher.utils
 
 import android.content.Context
 import android.content.pm.PackageManager
+import androidx.compose.ui.graphics.Color
+import com.example.android_launcher.domain.models.EventRecurringType
+import com.example.android_launcher.domain.models.Priority
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
+import kotlin.String
 
 fun formatTime(milliSecs: Long?): String {
     if (milliSecs == null || milliSecs <= 0) return ""
@@ -66,7 +71,36 @@ fun isAppInstalled(context: Context, packageName: String): Boolean {
         false
     }
 }
-fun formatTimeFromMillis(timeInMillis: Long): String {
-    val formatter = SimpleDateFormat("h:mm a", Locale.getDefault())
-    return formatter.format(Date(timeInMillis))
+fun LocalTime.formatTimeToRequiredFormat(timeFormat: String? = "24hr"): String {
+    return when (timeFormat) {
+        "24hr" -> String.format(Locale.getDefault(),"%02d:%02d hrs", hour, minute)
+        "12hr" -> {
+            val hour12 = when {
+                hour == 0 -> 12        // midnight
+                hour > 12 -> hour - 12 // afternoon
+                else -> hour           // 1–12 AM
+            }
+            val amPm = if (hour < 12) "am" else "pm"
+            String.format(Locale.getDefault(), "%02d:%02d%s", hour12, minute, amPm)
+        }
+        else -> ""
+    }
 }
+val repeatOptions = mapOf<EventRecurringType, String>(
+    EventRecurringType.NONE to "No repeat",
+    EventRecurringType.WEEK_DAYS to  "Week days",
+    EventRecurringType.SPECIFIC_DAYS_WEEKLY to "The selected days weekly"
+)
+val notifyOptions = mapOf<Int, String>(
+    5 to "5 minutes before",
+    10 to "10 minutes before",
+    15 to "15 minutes before",
+    30 to "30 minutes before",
+    60 to "1 hour before",
+)
+val priorityColors = mapOf<Priority, Color>(
+    Priority.LOW to Color(0xff4EFF57),
+    Priority.MEDIUM to Color(0xffFBC02D),
+    Priority.HIGH to Color(0xffF57C00),
+    Priority.HIGHEST to Color(0xffD32F2F)
+)
