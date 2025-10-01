a3d8ff5c69449c5f6922f05f51838f4c2b992351
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-10-01 10:09:47 +0300

    refactor: adds its own converter

--- app/src/main/java/com/example/android_launcher/domain/models/App.kt
+++ app/src/main/java/com/example/android_launcher/domain/models/App.kt
@@ -3,15 +3,19 @@ package com.example.android_launcher.domain.models
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.android_launcher.data.local.CommaListConverter
 
 
 @Entity(tableName = "apps", indices = [Index(value = ["packageName"], unique = true)])
+@TypeConverters(CommaListConverter::class)
 data class App(
     val name: String,
     @PrimaryKey val packageName: String,
     val isBlocked: Boolean?=false,
     val isPinned: Boolean?=false,
     val blockReleaseDate: String?= "",
+    val domains: List<String>,
     val isHidden: Boolean?=false,
     val category: String?=null,
 )
