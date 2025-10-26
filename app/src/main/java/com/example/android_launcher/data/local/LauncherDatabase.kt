c8c9aacc97b3479384560c15af30a1a54b3f2514
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-10-26 05:35:32 +0300

    refactor: separates the list converter

--- app/src/main/java/com/example/android_launcher/data/local/LauncherDatabase.kt
+++ app/src/main/java/com/example/android_launcher/data/local/LauncherDatabase.kt
@@ -7,7 +7,6 @@ import com.example.android_launcher.domain.models.App
 import com.example.android_launcher.domain.models.Event
 
 @Database(entities = [Event::class, App::class], version = 1)
-@TypeConverters(ListTypeConverter::class)
 abstract class LauncherDatabase: RoomDatabase() {
     abstract fun appsDao(): AppsDao
     abstract fun calendarDao(): CalendarDao
