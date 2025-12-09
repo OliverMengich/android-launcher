c8881e2ec5666bd36e21ecc08a25d18e3c78597a
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-12-09 12:43:32 +0300

    chore: functions rename

--- app/src/main/java/com/example/android_launcher/domain/repository/CalendarRepository.kt
+++ app/src/main/java/com/example/android_launcher/domain/repository/CalendarRepository.kt
@@ -1,9 +1,12 @@
 package com.example.android_launcher.domain.repository
 
 import com.example.android_launcher.domain.models.Event
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 
 interface CalendarRepository {
-    suspend fun getTodayEvents(startDay: Long): List<Event>
+    fun getTodayEvents(startDay: LocalDate): Flow<List<Event>>
+    suspend fun updateEvent(event: Event)
     suspend fun deleteEvent(id: Int)
     suspend fun insertEvent(event: Event)
 }
