abb3ba1f8229c8c6ca598a3122e551bd5b039488
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-12-11 09:17:12 +0300

    refactor: updates fetching todays events

--- app/src/main/java/com/example/android_launcher/data/local/CalendarDao.kt
+++ app/src/main/java/com/example/android_launcher/data/local/CalendarDao.kt
@@ -5,8 +5,11 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.android_launcher.domain.models.Event
 import com.example.android_launcher.domain.models.EventRecurringType
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 
 @Dao
 interface CalendarDao {
@@ -22,22 +25,36 @@ interface CalendarDao {
     @Query(value="SELECT * FROM events WHERE recurringType=:recurringType;")
     fun getDailyRecurringEvents(recurringType: EventRecurringType = EventRecurringType.DAILY): List<Event>
 
+
     @Query(value="""
         SELECT * FROM events
-        WHERE recurringType= :dailyRecurringType
-        OR (startDate >= :startDate AND endDate <= :endDate)
-        OR (recurringType=:specificDaysRecurring AND dates LIKE '%[' || :startDate || ']%')
-        OR( recurringType=:weekDaysRecurring AND :isWeekDay=1)
-        ORDER BY startDate ASC
+        WHERE (
+            recurringType= :dailyRecurringType 
+            AND endDate IS NULL OR endDate >= :todayDate 
+        )
+        OR (recurringType=:selectedDatesRecurring AND dates LIKE '%[' || :date || ']%')
+        OR(recurringType=:specificDaysWeeklyRecurring AND dates LIKE '%[' || :dayName || ']%')
+        OR(
+            recurringType=:weekDaysRecurring
+            AND :isWeekDay=1 
+            AND (endDate IS NULL OR endDate >= :todayDate)
+        )
+        ORDER BY startTime ASC
     """)
     fun getTodayEvents(
-        startDate: Long,
-        endDate: Long,
+        date: String,
+        dayName: String?="",
+        todayDate: String?= LocalDate.now().toString(),
         dailyRecurringType: EventRecurringType? = EventRecurringType.DAILY,
         weekDaysRecurring: EventRecurringType? = EventRecurringType.WEEK_DAYS,
-        specificDaysRecurring: EventRecurringType? = EventRecurringType.WEEK_DAYS,
+        specificDaysWeeklyRecurring: EventRecurringType?= EventRecurringType.SPECIFIC_DAYS_WEEKLY,
+        selectedDatesRecurring: EventRecurringType? = EventRecurringType.SELECTED_DATES,
         isWeekDay: Boolean,
-    ): List<Event>
+    ): Flow<List<Event>>
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun updateEvent(event: Event)
+
 
     @Query(value="DELETE FROM events WHERE id=:id")
     fun deleteEvent(id: Int)
