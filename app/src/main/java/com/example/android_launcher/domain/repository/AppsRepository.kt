fdea07258274524e722d098dd3ce106eb0b72fa8
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-12-14 04:46:21 +0300

    feat: changes to flows

--- app/src/main/java/com/example/android_launcher/domain/repository/AppsRepository.kt
+++ app/src/main/java/com/example/android_launcher/domain/repository/AppsRepository.kt
@@ -2,11 +2,14 @@ package com.example.android_launcher.domain.repository
 
 import com.example.android_launcher.domain.models.App
 import com.example.android_launcher.domain.models.BatteryInfo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 interface AppsRepository {
     val batteryInfo: MutableStateFlow<BatteryInfo?>
-    fun getAllApps(): List<App>
+    val refetchAppsFlow: MutableStateFlow<Boolean?>
+    fun getAllApps(): Flow<List<App>>
     fun insertApps(apps: List<App>)
     fun getPinnedApps(): List<App>
     fun getHiddenApps(): List<App>
