0515e06d849aec4febae657438a2cb01dc110b46
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-12-17 13:10:11 +0300

    feat: fetches login state from datastore, removes shared prefs

--- app/src/main/java/com/example/android_launcher/LauncherActivity.kt
+++ app/src/main/java/com/example/android_launcher/LauncherActivity.kt
@@ -1,27 +1,22 @@
 package com.example.android_launcher
 
-import android.content.Context
+
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.core.content.ContextCompat
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.android_launcher.services.AppMonitorService
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlinx.coroutines.launch
 
 class LauncherActivity: ComponentActivity() {
-    private val mainViewModel: MainViewModel by viewModel()
+//    private val mainViewModel: MainViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        installSplashScreen().setKeepOnScreenCondition {
@@ -32,21 +27,31 @@ class LauncherActivity: ComponentActivity() {
         ContextCompat.startForegroundService(this, intent)
         enableImmersiveMode()
 
-        val sharedRef = getSharedPreferences("settings_value", Context.MODE_PRIVATE)
-        val isLoggedInUser = sharedRef.getBoolean("IS_AUTHENTICATED",false)
-        Log.d("start_intent"," is user logged in=$isLoggedInUser")
-        val startIntent = if (isLoggedInUser){
-            Intent(this, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        lifecycleScope.launch {
+            val localManager = dataStore.data.first()
+            val startIntent = if (localManager.isLoggedIn) {
+                Intent(this@LauncherActivity, MainActivity::class.java)
+            } else {
+                Intent(this@LauncherActivity, OnboardingActivity::class.java)
             }
-        }else{
-            Intent(this, OnboardingActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
+            startActivity(startIntent)
+            finish()
         }
-        Log.d("start_intent","start intent is $startIntent")
-        startActivity(startIntent)
-        finish()
+//        val sharedRef = getSharedPreferences("settings_value", Context.MODE_PRIVATE)
+//        val isLoggedInUser = sharedRef.getBoolean("IS_AUTHENTICATED",false)
+//        Log.d("start_intent"," is user logged in=$isLoggedInUser")
+//        val startIntent = if (isLoggedInUser){
+//            Intent(this, MainActivity::class.java).apply {
+//                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//            }
+//        }else{
+//            Intent(this, OnboardingActivity::class.java).apply {
+//                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//            }
+//        }
+//        Log.d("start_intent","start intent is $startIntent")
+//        startActivity(startIntent)
+//        finish()
     }
     private fun enableImmersiveMode(){
         val windowInsetsController = WindowCompat.getInsetsController(window,window.decorView)
