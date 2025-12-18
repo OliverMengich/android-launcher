f3dab267f1bdf149b34e286ed38ab83179da8099
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-12-18 08:40:11 +0300

    ffeat: changes welcome message

--- app/src/main/java/com/example/android_launcher/presentation/screens/onboarding/components/Welcome.kt
+++ app/src/main/java/com/example/android_launcher/presentation/screens/onboarding/components/Welcome.kt
@@ -1,6 +1,7 @@
 package com.example.android_launcher.presentation.screens.onboarding.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,6 +14,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,45 +22,64 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.android_launcher.R
 
 @Composable
 fun WelcomeScreen(navigateToNext: ()-> Unit){
-    Column (modifier = Modifier.fillMaxSize()){
+    Column(
+        modifier = Modifier.fillMaxSize().padding(start = 24.dp, end = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = "Welcome to Planara Launcher",
+            fontWeight = FontWeight.ExtraBold, fontSize = 25.sp,
+            modifier = Modifier.padding(top = 0.dp)
+        )
         Image(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.7f),
-            painter = painterResource(id = R.drawable.onboarding1),
-            contentDescription = "null",
-            contentScale = ContentScale.Crop
+            painter = painterResource(id = R.drawable.planara_icon),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth().height(300.dp),
+            contentScale = ContentScale.FillBounds
         )
-
-        Spacer(modifier = Modifier.height(36.dp))
-        Text(
-            text = "Welcome",
-            modifier = Modifier.padding(10.dp),
-            style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.Bold),
-        )
-        Text(
-            text = "Lorem ipsum",
-            modifier = Modifier.padding(10.dp),
-            style = MaterialTheme.typography.bodyMedium,
-        )
-        Button(
-            modifier = Modifier.align(Alignment.End).padding(horizontal = 20.dp),
-            onClick = navigateToNext,
-            colors = ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.primary,
-                contentColor = Color.White
-            ),
-            shape = RoundedCornerShape(size = 6.dp)
-        ) {
+        Column(Modifier.fillMaxWidth()){
             Text(
-                text = "NEXT",
-                style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
+                text = "Planara launcher is a productivity tool that helps declutter your phone, plan your day, " +
+                        "and boost productivity. Manage tasks," +
+                        "schedule events, and stay organized—all from a clean, distraction-free home screen.",
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    lineHeight = 22.sp,
+                    textAlign = TextAlign.Start
+                ),
             )
+            TextButton(
+                onClick = navigateToNext,
+                modifier = Modifier.align(Alignment.End).padding(all = 20.dp),
+                content = {
+                    Text(
+                        text="GET STARTED",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(10.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontSize = 15.sp,
+                    )
+                }
+            )
+//            TextButton(
+//                modifier = Modifier.align(Alignment.End).padding(all = 20.dp),
+//                onClick = navigateToNext,
+//                shape = RoundedCornerShape(size = 6.dp)
+//            ) {
+//                Text(
+//                    text = "NEXT",
+//                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
+//                )
+//            }
         }
     }
 }
