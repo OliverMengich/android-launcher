5dca36797cc8b92a45489d86fa5eb805a2ac8fad
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-12-20 09:20:11 +0300

    feat: update and delete event

--- app/src/main/java/com/example/android_launcher/presentation/screens/home/home/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/example/android_launcher/presentation/screens/home/home/calendar/CalendarViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.android_launcher.presentation.screens.home.home.calendar
 
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android_launcher.domain.models.Event
@@ -8,11 +10,13 @@ import com.example.android_launcher.domain.models.EventRecurringType
 import com.example.android_launcher.domain.models.Priority
 import com.example.android_launcher.domain.repository.CalendarRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -30,13 +34,13 @@ data class NewEventPageState(
     val eventName: String="",
     val locationLink: String = "",
     val description: String = "",
-    val isAllDay: Boolean = false,
+    val notifyBeforeTime: Int=30,
     val selectedDates: Set<LocalDate> = emptySet<LocalDate>(),
     val startTime: LocalTime= LocalTime.now().plusMinutes(30),
     val endTime: LocalTime= LocalTime.now().plusHours(1),
     val eventCategory: EventCategory = EventCategory.EVENT,
     val selectedPriority: Priority = Priority.LOW,
-    val selectedRecurringType : EventRecurringType = EventRecurringType.NONE
+    val selectedRecurringType : EventRecurringType = EventRecurringType.SELECTED_DATES
 )
 sealed class NewEventPageEvent{
     data class UpdateEventPageValue<T>(
@@ -47,10 +51,9 @@ sealed class NewEventPageEvent{
     data object CreateEvent: NewEventPageEvent()
     data class ShowErrorMessage(val message: String): NewEventPageEvent()
     data object ShowSuccess: NewEventPageEvent()
-
 }
-
-class CalendarViewModel(private val calendarRepository: CalendarRepository): ViewModel() {
+@OptIn(ExperimentalCoroutinesApi::class)
+class CalendarViewModel(private val calendarRepository: CalendarRepository, private val context: Context): ViewModel() {
     private val _newEventPageState = MutableStateFlow<NewEventPageState>(NewEventPageState())
     val newEventPageState: StateFlow<NewEventPageState> = _newEventPageState.stateIn(
         scope = viewModelScope,
@@ -61,25 +64,24 @@ class CalendarViewModel(private val calendarRepository: CalendarRepository): Vie
     val dates: StateFlow<List<List<LocalDate>>> = _dates.asStateFlow()
 
     private val _todayEvents = MutableStateFlow<List<Event>>(emptyList())
-    val todayEvents: StateFlow<List<Event>> = _todayEvents.asStateFlow()
+//    val todayEvents: StateFlow<List<Event>> = _todayEvents.asStateFlow()
+    private val startDate = MutableStateFlow<LocalDate>(LocalDate.now())
+
+
+    val todayEvents = calendarRepository.getTodayEvents(startDay = LocalDate.now()).stateIn(viewModelScope, SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000), emptyList())
+    val dateEvents = startDate.flatMapLatest { day->
+        calendarRepository.getTodayEvents(startDay = day)
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000), emptyList())
+
+    private val _dateEvents = MutableStateFlow<List<Event>>(emptyList())
+//    val dateEvents: StateFlow<List<Event>> = _dateEvents.asStateFlow()
 
     private val _newPageEvent: Channel<NewEventPageEvent> = Channel()
     val newPageEvent =_newPageEvent.receiveAsFlow()
 
-    init {
-        viewModelScope.launch(context=Dispatchers.IO) {
-            val calendar = Calendar.getInstance()
-            val startDay = calendar.apply {
-                set(Calendar.HOUR_OF_DAY,0)
-                set(Calendar.MINUTE,0)
-                set(Calendar.SECOND,0)
-                set(Calendar.SECOND,0)
-                set(Calendar.MILLISECOND,0)
-            }.timeInMillis
-            getTodayEvents(startDay)
-        }
-    }
-
+//    init {
+//        getTodayEvents()
+//    }
     fun onEvent(event: NewEventPageEvent){
         when(event){
             is NewEventPageEvent.UpdateEventPageValue<*> -> {
@@ -88,9 +90,16 @@ class CalendarViewModel(private val calendarRepository: CalendarRepository): Vie
                         NewEventPageState::eventName -> currentState.copy(eventName = event.value as String)
                         NewEventPageState::locationLink-> currentState.copy(locationLink = event.value as String)
                         NewEventPageState::description -> currentState.copy(description = event.value as String)
-                        NewEventPageState::isAllDay -> currentState.copy(isAllDay = event.value as Boolean)
                         NewEventPageState::selectedDates -> currentState.copy(selectedDates = event.value as Set<LocalDate>)
                         NewEventPageState::selectedPriority -> currentState.copy(selectedPriority = event.value as Priority)
+                        NewEventPageState::startTime -> {
+                            currentState.copy(
+                                startTime = event.value as LocalTime,
+                                endTime = LocalTime.of(event.value.hour, event.value.minute).plusMinutes(30)
+                            )
+                        }
+                        NewEventPageState::endTime -> currentState.copy(endTime = event.value as LocalTime)
+                        NewEventPageState::notifyBeforeTime -> currentState.copy(notifyBeforeTime = event.value as Int)
                         NewEventPageState::eventCategory -> currentState.copy(eventCategory =  event.value as EventCategory)
                         NewEventPageState::selectedRecurringType -> currentState.copy(selectedRecurringType =  event.value as EventRecurringType)
                         else -> currentState
@@ -107,27 +116,27 @@ class CalendarViewModel(private val calendarRepository: CalendarRepository): Vie
                     EventRecurringType.NONE,
                     EventRecurringType.DAILY,
                     EventRecurringType.WEEK_DAYS,
-                    EventRecurringType.SPECIFIC_DATES -> {
+                    EventRecurringType.SELECTED_DATES -> {
                         newEventPageState.value.selectedDates.map { it.toString() }
                     }
                 }
                 viewModelScope.launch {
-                    val (eventName, locationLink, description, isAllDay, selectedDates,startTime,endTime, eventCategory, selectedPriority, selectedRecurringType) = newEventPageState.value
+                    val (eventName, locationLink, description, notifyBeforeTime, _, startTime,endTime, eventCategory, selectedPriority, selectedRecurringType) = newEventPageState.value
+                    Toast.makeText(context, "Creating event $selectedPriority", Toast.LENGTH_SHORT).show()
                     newEvent(
                         e=Event(
-                            id = Random.nextInt(0,1000_000),
                             title =eventName,
                             location = locationLink,
-                            description = description.take(n=20),
+                            description = description,
                             notesPath = "",
-                            startTime = startTime?.toSecondOfDay()?.toLong() ,
-                            endTime = endTime?.toSecondOfDay()?.toLong(),
+                            startTime = startTime,
+                            endTime = endTime,
                             notesUrl = "",
                             eventCategory = eventCategory,
-                            priority = Priority.LOW,
+                            priority = selectedPriority,
                             dates = finalDates,
-                            isPassed = false,
                             recurringType = selectedRecurringType,
+                            notifyBeforeTime = notifyBeforeTime
                         )
                     )
                 }
@@ -135,9 +144,18 @@ class CalendarViewModel(private val calendarRepository: CalendarRepository): Vie
             else -> {}
         }
     }
-    suspend fun getTodayEvents(startDay: Long){
-        val tdEvs = calendarRepository.getTodayEvents(startDay)
-        _todayEvents.value = tdEvs
+//    fun getTodayEvents(){
+//        viewModelScope.launch(context=Dispatchers.IO) {
+//            val tdEvs = calendarRepository.getTodayEvents(startDay = LocalDate.now())
+//            _todayEvents.value = tdEvs
+//        }
+//    }
+    fun getDateEvents(startDay: LocalDate){
+        startDate.value = startDay
+//        viewModelScope.launch(context=Dispatchers.IO) {
+//            val tdEvs = calendarRepository.getTodayEvents(startDay)
+//            _dateEvents.value = tdEvs
+//        }
     }
 
     fun generateDays(dt: LocalDate?) {
@@ -156,8 +174,31 @@ class CalendarViewModel(private val calendarRepository: CalendarRepository): Vie
         try {
             calendarRepository.insertEvent(e)
             _newPageEvent.send(NewEventPageEvent.ShowSuccess)
+//            getTodayEvents()
         }catch (e: Exception){
             _newPageEvent.send(NewEventPageEvent.ShowErrorMessage("Could create event: ${e.message}"))
+        }
+    }
+    fun updateEvent(e: Event){
+        viewModelScope.launch(context=Dispatchers.IO) {
+            try {
+                calendarRepository.updateEvent(event=e)
+                _newPageEvent.send(NewEventPageEvent.ShowSuccess)
+//                getTodayEvents()
+            } catch (e: Exception) {
+                Toast.makeText(context, "Could update event: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    fun deleteEvent(eventId: Int){
+        viewModelScope.launch(context=Dispatchers.IO)  {
+            try {
+                calendarRepository.deleteEvent(eventId)
+                _newPageEvent.send(NewEventPageEvent.ShowSuccess)
+//                getTodayEvents()
+            } catch (e: Exception) {
+                Toast.makeText(context, "Could delete event: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
     private fun eachWeekUntil(start: LocalDate, end: LocalDate): List<List<LocalDate>> {
