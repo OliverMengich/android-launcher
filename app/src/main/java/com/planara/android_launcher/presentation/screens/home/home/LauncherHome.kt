08071d5b689d418fe7f14fec1a9179e275113091
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2026-06-14 08:18:20 +0300

    feat: watches active tab from global state

--- app/src/main/java/com/planara/android_launcher/presentation/screens/home/home/LauncherHome.kt
+++ app/src/main/java/com/planara/android_launcher/presentation/screens/home/home/LauncherHome.kt
@@ -1,5 +1,7 @@
 package com.planara.android_launcher.presentation.screens.home.home
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -17,21 +19,50 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.planara.android_launcher.R
+import com.planara.android_launcher.dataStore
+import com.planara.android_launcher.domain.manager.LocalManager
 import com.planara.android_launcher.domain.models.App
 import com.planara.android_launcher.presentation.screens.home.home.apps.AppsPage
 import com.planara.android_launcher.presentation.screens.home.home.calendar.CalendarPage
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 @Composable
 fun LauncherHome(navigateToSettingsPage:()->Unit, navigateToBlockedApp:(App)->Unit,navigateToNewEvent:()->Unit, navigateToBlockingAppPage:(App)-> Unit) {
-    val pagerState = rememberPagerState(initialPage = 1, pageCount = { 3 })
+    val ctx = LocalContext.current
+
+    val prefs by ctx.dataStore.data.collectAsState(
+        initial = LocalManager()
+    )
+
+    val pagerState = rememberPagerState(initialPage = prefs.activeTab, pageCount = { 3 })
     val scope = rememberCoroutineScope()
+    LaunchedEffect(prefs.activeTab) {
+        pagerState.animateScrollToPage(prefs.activeTab)
+    }
+    LaunchedEffect(pagerState) {
+        snapshotFlow { pagerState.currentPage }
+            .collect { page ->
+                ctx.dataStore.updateData {
+                    it.copy(activeTab = page)
+                }
+            }
+    }
     Scaffold(
         modifier = Modifier.fillMaxSize().background(Color.Black),
         bottomBar = {
